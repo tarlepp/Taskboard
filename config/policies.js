dddb8e1ac31621e14fd29ed04cb3903b53d4d8a6
tarlepp <tarmo.leppanen@protacon.fi>
2013-06-25 17:50:53 +0300

    Fixed tabs to spaces

--- config/policies.js
+++ config/policies.js
@@ -1,21 +1,19 @@
 /**
-* Policy defines middleware that is run before each controller/controller.
-* Any policy dropped into the /middleware directory is made globally available through sails.middleware
-* Below, use the string name of the middleware
-*/
+ * Policy defines middleware that is run before each controller/controller.
+ * Any policy dropped into the /middleware directory is made globally available through sails.middleware
+ * Below, use the string name of the middleware
+ */
 module.exports.policies = {
+    // Default policy (allow public access)
+    '*': true
 
-	// Default policy (allow public access)
-	'*': true
+    /** Example mapping:
+    someController: {
+        // Apply the "authenticated" policy to all actions
+        '*': 'authenticated',
 
-	/** Example mapping: 
-	someController: {
-
-		// Apply the "authenticated" policy to all actions
-		'*': 'authenticated',
-
-		// For someAction, apply 'somePolicy' instead
-		someAction: 'somePolicy'
-	}
-	*/
+        // For someAction, apply 'somePolicy' instead
+        someAction: 'somePolicy'
+    }
+    */
 };
--- config/views.js
+++ config/views.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'viewEngine': 'ejs'
+    'viewEngine': 'ejs'
 };
