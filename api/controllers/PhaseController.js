23b2dbf1ef304da7cbcb80da808862216fb9a993
tarlepp <tarmo.leppanen@kase.fi>
2013-07-24 13:57:13 +0300

    Refactored phase controller.

--- api/controllers/PhaseController.js
+++ api/controllers/PhaseController.js
@@ -4,8 +4,13 @@
  * @module      ::  Controller
  * @description ::  Contains logic for handling requests.
  */
-
 module.exports = {
+    /**
+     * Project phase edit action.
+     *
+     * @param req
+     * @param res
+     */
     edit: function(req, res) {
         if (!req.isAjax) {
             res.send('Only AJAX request allowed', 403);
@@ -13,36 +18,61 @@ module.exports = {
 
         var projectId = parseInt(req.param('id'), 10);
 
-        Project.findOne(projectId)
+        // Specify template data to use
+        var data = {
+            layout: "layout_ajax",
+            project: false,
+            phases: false
+        };
+
+        // Fetch project data.
+        Project
+        .findOne(projectId)
         .done(function(error, project) {
             if (error) {
                 res.send(error, 500);
+            } else if (!project) {
+                res.send("Project not found.", 404);
             } else {
-                fetchPhases(project);
+                data.project = project;
+
+                makeView();
             }
         });
 
-        function fetchPhases(project) {
-            Phase.find()
-            .where({
-                projectId: project.id
-            })
-            .sort('order ASC')
-            .done(function(error, phases) {
-                if (error) {
-                    res.send(error, 500);
-                } else {
-                    makeView(project, phases);
+        // Fetch project phases
+        Phase
+        .find()
+        .where({
+            projectId: projectId
+        })
+        .sort('order ASC')
+        .done(function(error, phases) {
+            if (error) {
+                res.send(error, 500);
+            } else {
+                data.project = phases;
+
+                makeView();
+            }
+        });
+
+        /**
+         * Function makes actual view if all necessary data is fetched
+         * from database for template.
+         */
+        function makeView() {
+            var ok = true;
+
+            jQuery.each(data, function(key, data) {
+                if (data === false) {
+                    ok = false;
                 }
             });
-        }
 
-        function makeView(project, phases) {
-            return res.view({
-                layout: "layout_ajax",
-                project: project,
-                phases: phases
-            });
+            if (ok) {
+                res.view(data);
+            }
         }
     }
 };
