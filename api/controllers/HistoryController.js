15804d376bd3b9b69a5757c3fc9bcb97665f50e0
tarlepp <tarmo.leppanen@kase.fi>
2013-09-08 03:53:56 +0300

    Implemented object history data view for specified object.

--- api/controllers/HistoryController.js
+++ api/controllers/HistoryController.js
@@ -4,6 +4,8 @@
  * @module      ::  Controller
  * @description ::  Contains logic for handling requests.
  */
+var jQuery = require('jquery');
+var objectDiff = require('objectdiff');
 
 module.exports = {
     /**
@@ -15,7 +17,193 @@ module.exports = {
     index: function(req, res) {
         var objectId = req.param('objectId');
         var objectName = req.param('objectName');
+        var data = [];
 
-        // Todo implement actual functionality in next phase.
+        History
+            .find()
+            .where({
+                objectName: objectName,
+                objectId: objectId
+            })
+            .sort('id ASC')
+            .done(function(error, data) {
+                var histories = [];
+
+                // Remove duplicate rows
+                jQuery.each(data, function(key, row) {
+                    if (key === 0 || row.objectData !== data[key - 1].objectData) {
+                        histories.push(row);
+                    }
+                });
+
+                // Process history data
+                processHistoryData(histories);
+            });
+
+        /**
+         * Function process history data and fetches possible relation data
+         * from models.
+         *
+         * Please note that this function is very complicated so be very
+         * carefully when you change this functionality - good luck.
+         *
+         * @param   {sails.helper.history[]}  histories
+         */
+        function processHistoryData(histories) {
+            var dataCount = histories.length;
+
+            // First call makeView, this is needed for objects that have no history
+            makeView();
+
+            // Iterate history data
+            jQuery.each(histories, function(key, history) {
+                var historyRow;
+
+                // First record, assume that object is created at this point
+                if (key === 0) {
+                    historyRow = {
+                        message: 'Object created',
+                        stamp: history.createdAt,
+                        data: []
+                    };
+
+                    data.push(historyRow);
+
+                    makeView();
+                } else { // Otherwise we have some object data updated
+                    historyRow = {
+                        message: 'Object updated',
+                        stamp: history.createdAt,
+                        data: []
+                    };
+
+                    // Calculate object difference to previous one
+                    var diff = objectDiff.diff(JSON.parse(histories[key - 1].objectData), JSON.parse(history.objectData));
+
+                    // Initialize attribute change count
+                    var changeCount = 0;
+
+                    // First calculate how many attribute(s) have been changed
+                    jQuery.each(diff.value, function(column, value) {
+                        if (value.changed != 'equal') {
+                            changeCount++;
+                        }
+                    });
+
+                    // Iterate each attributes, note that this contains also attributes that have not been changed
+                    jQuery.each(diff.value, function(column, value) {
+                        // Attribute value has been changed
+                        if (value.changed != 'equal') {
+                            // Initialize values
+                            var valueOld = false;
+                            var valueNew = false;
+                            var objectIdNew = false;
+                            var objectIdOld = false;
+
+                            /**
+                             * Magic happens here, we can assume that attributes which ends with 'Id'
+                             * strings are actual relations to another models.
+                             *
+                             * In these cases we want to fetch actual text values for changed old and
+                             * new values.
+                             *
+                             * Note that fetching old and new value must be done at in same process
+                             * otherwise this doesn't work right.
+                             */
+                            if (value.changed != 'equal' && column.match(/Id$/) !== null) {
+                                var object = column.charAt(0).toUpperCase() + column.slice(1, -2);
+
+                                // Store actual id values
+                                objectIdNew = value.added;
+                                objectIdOld = value.removed;
+
+                                // Fetch new value
+                                global[object]
+                                    .findOne(objectIdNew)
+                                    .done(function(error, objectData) {
+                                        // Store real new value
+                                        valueNew = (!objectData) ? "none" : objectData.objectTitle();
+
+                                        // Fetch old value
+                                        global[object]
+                                            .findOne(objectIdOld)
+                                            .done(function(error, objectData) {
+                                                // Store real old value
+                                                valueOld = (!objectData) ? "none" : objectData.objectTitle();
+
+                                                // Make necessary checks if we can show page or not
+                                                checkData();
+                                            });
+                                    });
+                            } else { // No relation
+                                valueOld = value.removed.length == 0 ? 'empty' : value.removed;
+                                valueNew = value.added.length == 0 ? 'empty' : value.added;
+
+                                checkData();
+                            }
+
+                            /**
+                             * Private function to check that we have processed all required
+                             * data for view.
+                             *
+                             * This function is called x times in this action.
+                             */
+                            function checkData() {
+                                var ready = false;
+
+                                // Add data to history row object
+                                historyRow.data.push({
+                                    column: column,
+                                    valueNew: valueNew,
+                                    valueOld: valueOld,
+                                    valueIdOld: objectIdOld,
+                                    valueIdNew: objectIdNew
+                                });
+
+                                // Check if have processed all required rows
+                                if (changeCount == historyRow.data.length) {
+                                    ready = true;
+
+                                    // Add data to view object
+                                    data.push(historyRow);
+                                }
+
+                                if (ready) {
+                                    makeView();
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+
+            /**
+             * This function will make actual view to client.
+             */
+            function makeView() {
+                var ready = data.length === dataCount;
+
+                // We're ready to show some stuff
+                if (ready) {
+                    // Data sorting function
+                    function sortByStamp(a, b) {
+                        if (a.stamp < b.stamp)
+                            return 1;
+                        if (a.stamp > b.stamp)
+                            return -1;
+                        return 0;
+                    }
+
+                    // Sort data
+                    data.sort(sortByStamp);
+
+                    // Make view
+                    res.view({
+                        layout: 'layout_ajax',
+                        data: data
+                    });
+                }
+            }
+        }
     }
 };
