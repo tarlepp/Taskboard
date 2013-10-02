76e59dbf8e858cb8f4d97afe2d110af248dc4e58
tarlepp <tarmo.leppanen@kase.fi>
2013-10-02 19:38:02 +0300

    Added global AJAX settings, basically just for error handling.

--- assets/linker/js/board.js
+++ assets/linker/js/board.js
@@ -9,6 +9,14 @@
  * Initialize all necessary javascript on document ready event.
  */
 jQuery(document).ready(function() {
+    // Global AJAX setup
+    jQuery.ajaxSetup({
+        error: function(jqXhr, textStatus, error) {
+            handleAjaxError(jqXhr, textStatus, error);
+        },
+        type: "post"
+    });
+
     // Global bootbox settings
     bootbox.setDefaults({
         animate: false
