246417d3d6634a7572440568c28f70676bea87d6
tarlepp <tarmo.leppanen@kase.fi>
2013-10-05 01:38:02 +0300

    Support only chrome...

--- assets/linker/js/board.js
+++ assets/linker/js/board.js
@@ -24,4 +24,29 @@ jQuery(document).ready(function() {
 
     // Global tooltip event
     initTooltips(jQuery(document));
+
+    // We only accept chrome, I know this will be a problem...
+    if (typeof window.chrome === "undefined") {
+        makeBrowserError();
+    }
+
+    /**
+     * Function to show browser error. This is used if user don't use
+     * Google Chrome browser. Note that if user dismiss the noty dialog
+     * it is just triggered again and again and again...
+     */
+    function makeBrowserError() {
+        var message = "Please use <em><a href='https://www.google.com/chrome/' target='_blank'>Google Chrome browser</a></em>, otherwise we can not guarantee user experience.";
+
+        makeMessage(message, "error", {
+            timeout: 0,
+            force: true,
+            template: "<div class='noty_message'><span class='noty_text'></span></div>",
+            callback: {
+                onClose: function() {
+                    makeBrowserError();
+                }
+            }
+        });
+    }
 });
