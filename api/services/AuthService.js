c1cd78ded087c2d6794fdd801f93cf490b1c6ca5
tarlepp <tarmo.leppanen@kase.fi>
2013-10-09 19:19:10 +0300

    Fixed logic of callback and added missing comments.

--- api/services/AuthService.js
+++ api/services/AuthService.js
@@ -8,18 +8,28 @@ var async = require("async");
 /**
  * Method checks if specified user has access to specified project or not.
  *
- * @param   {User}      user      Signed in user id
+ * @param   {User}      user        Signed in user object
  * @param   {Number}    projectId   Project id to check
+ * @param   {Function}  next        Main callback function, which is called after checks
  */
 exports.hasAccessToProject = function(user, projectId, next) {
     // Admin user, always true
     if (user.admin) {
         next(null, true);
     } else { // Otherwise fetch user role
-        async.parallel({
+        /**
+         * Make parallel jobs to determine if user has access to specified project or not.
+         * Basically we check if user is
+         *  a) primary project manager
+         *  b) attached to project in some role
+         *
+         * If current user is either of these provided next callback is called by false.
+         */
+        async.parallel(
+            {
                 /**
-                 * Function to fetch possible Project object for signed in user and
-                 * specified project. User must be project manager.
+                 * Function to fetch possible Project object for signed in user and specified project.
+                 * User must be project manager.
                  *
                  * Note that this query may return undefined.
                  *
@@ -41,8 +51,7 @@ exports.hasAccessToProject = function(user, projectId, next) {
                 },
 
                 /**
-                 * Function to fetch possible ProjectUser object for signed in user
-                 * and specified project.
+                 * Function to fetch possible ProjectUser object for signed in user and specified project.
                  *
                  * Note that this query may return undefined.
                  *
