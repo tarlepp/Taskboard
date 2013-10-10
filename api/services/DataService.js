664c2a816b4a1fff9c63e041315047d2e2c01a9b
tarlepp <tarmo.leppanen@kase.fi>
2013-10-10 21:00:48 +0300

    Implemented necessary credential checks to MilestoneController.

--- api/services/DataService.js
+++ api/services/DataService.js
@@ -14,7 +14,7 @@
 exports.getProject = function(projectId, callback) {
     Project
         .findOne(projectId)
-        .done(function(error, project) {
+        .done(function(error, /** sails.model.project */ project) {
             if (error) {
                 callback(error, null);
             } else if (!project) {
@@ -39,10 +39,10 @@ exports.getProject = function(projectId, callback) {
 exports.getSprint = function(sprintId, callback) {
     Sprint
         .findOne(sprintId)
-        .done(function(error, project) {
+        .done(function(error, /** sails.model.sprint */ sprint) {
             if (error) {
                 callback(error, null);
-            } else if (!project) {
+            } else if (!sprint) {
                 var err = new Error();
 
                 err.message = "Sprint not found.";
@@ -50,7 +50,32 @@ exports.getSprint = function(sprintId, callback) {
 
                 callback(err, null);
             } else {
-                callback(null, project);
+                callback(null, sprint);
+            }
+        });
+};
+
+/**
+ * Service to fetch single milestone data from database.
+ *
+ * @param   {Number}    milestoneId Milestone id
+ * @param   {Function}  callback    Callback function to call after query
+ */
+exports.getMilestone = function(milestoneId, callback) {
+    Milestone
+        .findOne(milestoneId)
+        .done(function(error, /** sails.model.milestone */ milestone) {
+            if (error) {
+                callback(error, null);
+            } else if (!milestone) {
+                var err = new Error();
+
+                err.message = "Milestone not found.";
+                err.status = 404;
+
+                callback(err, null);
+            } else {
+                callback(null, milestone);
             }
         });
 };
