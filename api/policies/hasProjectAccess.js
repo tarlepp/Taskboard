7c4d3aa433c90a834dcc91c5c6ee511fe58a1151
tarlepp <tarmo.leppanen@kase.fi>
2013-10-12 00:56:25 +0300

    Added missing ;

--- api/policies/hasProjectAccess.js
+++ api/policies/hasProjectAccess.js
@@ -31,5 +31,5 @@ module.exports = function hasProjectAccess(request, response, next) {
 
             next();
         }
-    })
+    });
 };
--- api/policies/hasProjectUpdate.js
+++ api/policies/hasProjectUpdate.js
@@ -24,5 +24,5 @@ module.exports = function hasProjectUpdate(request, response, next) {
 
             next();
         }
-    })
+    });
 };
