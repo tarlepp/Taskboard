4ed6a09be740cd49215315784766d46edaaa467f
tarlepp <tarmo.leppanen@kase.fi>
2013-11-07 21:22:28 +0200

    Added new policies: isAjax, isSocket and isAjaxOrSocket.

---
+++ api/policies/isAjax.js
@@ -0,0 +1,25 @@
+// api/policies/isAjax.js
+
+/**
+ * Policy to check if current request is AJAX or not.
+ *
+ * @param   {Request}   request     Request object
+ * @param   {Response}  response    Response object
+ * @param   {Function}  next        Callback function to call if all is ok
+ */
+module.exports = function isAjax(request, response, next) {
+    sails.log.verbose(" POLICY - api/policies/isAjax.js");
+
+    if (request.isAjax) {
+        next();
+    } else {
+        setTimeout(function() {
+            /**
+             * TODO make message which is been shown to current user only. Is there some
+             *      "right" way to this kind of messaging?
+             */
+        }, 1000);
+
+        response.redirect("/");
+    }
+};
