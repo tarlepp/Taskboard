c7b3e50d525d2874cfa1869b6cb425894933cfa2
tarlepp <tarmo.leppanen@kase.fi>
2013-11-24 13:38:06 +0200

    Implemented automatic update of start and end times of tasks and stories.

--- api/models/Task.js
+++ api/models/Task.js
@@ -5,7 +5,8 @@
  * @description ::  Model represents single task on taskboard. Tasks are attached to specified user story, phase,
  *                  type and user.
  */
-var jQuery = require('jquery');
+var jQuery = require("jquery");
+var async = require("async");
 
 module.exports = {
     schema: true,
@@ -119,17 +120,34 @@ module.exports = {
      */
     beforeUpdate: function(values, cb) {
         if (values.phaseId) {
-            Phase
-                .findOne(values.phaseId)
-                .done(function(error, /** sails.model.phase */phase) {
+            async.parallel(
+                {
+                    // Fetch phase data
+                    phase: function(callback) {
+                        DataService.getPhase(values.phaseId, callback);
+                    },
+
+                    // Fetch task data
+                    task: function(callback) {
+                        DataService.getTask(values.id, callback);
+                    }
+                },
+                function (error, data) {
                     if (error) {
-                        cb(error)
+                        cb(error);
                     } else {
-                        values.isDone = (!phase) ? 0 : phase.isDone;
+                        values.isDone = data.phase.isDone;
+                        values.timeEnd = values.isDone ? new Date() : null;
+
+                        // We can assume that this update is from the board, so set start time if needed
+                        if (!data.task.timeStart || data.task.timeStart == '0000-00-00 00:00:00') {
+                            values.timeStart = new Date();
+                        }
 
                         cb();
                     }
-                });
+                }
+            );
         } else {
             cb();
         }
@@ -160,6 +178,7 @@ module.exports = {
                         .where({id: {'!': task.id}})
                         .done(function(error, /** sails.model.task[] */tasks) {
                             var isDone = true;
+                            var timeEnd = null;
 
                             if (_.size(tasks) > 0) {
                                 // Iterate story tasks
@@ -172,11 +191,15 @@ module.exports = {
                                 isDone = false;
                             }
 
+                            if (isDone) {
+                                timeEnd = new Date();
+                            }
+
                             // Update story data
                             Story
                                 .update(
                                     {id: task.storyId},
-                                    {isDone: isDone},
+                                    {isDone: isDone, timeEnd: timeEnd},
                                     function(error, /** sails.model.story[] */stories) {
                                         if (error) {
                                             cb(error);
@@ -202,7 +225,7 @@ module.exports = {
         Story
             .update(
                 {id: values.storyId},
-                {isDone: 0},
+                {isDone: 0, timeEnd: null},
                 function(error, /** sails.model.story[] */stories) {
                     if (error) {
                         cb(error);
@@ -224,31 +247,57 @@ module.exports = {
     afterUpdate: function(values, cb) {
         HistoryService.write('Task', values);
 
-        Task
-            .find()
-            .where({storyId: values.storyId})
-            .done(function(error, /** sails.model.task[] */tasks) {
-                var isDone = true;
+        async.parallel(
+            {
+                // Fetch phase data
+                story: function(callback) {
+                    DataService.getStory(values.storyId, callback);
+                },
 
-                // Iterate story tasks
-                jQuery.each(tasks, function(key, /** sails.model.task */task) {
-                    if (!task.isDone) {
-                        isDone = false;
+                // Fetch task data
+                task: function(callback) {
+                    DataService.getTask(values.id, callback);
+                },
+
+                tasks: function(callback) {
+                    DataService.getTasks({storyId: values.storyId}, callback);
+                }
+            },
+            function (error, data) {
+                if (error) {
+                    cb(error);
+                } else {
+                    var isDone = true;
+                    var timeEnd = new Date();
+
+                    _.each(data.tasks, function(task) {
+                        if (!task.isDone) {
+                            isDone = false;
+                            timeEnd = null;
+                        }
+                    });
+
+                    var where = {id: data.story.storyId};
+                    var updateData = {
+                        isDone: isDone,
+                        timeEnd: timeEnd
+                    };
+
+                    if ((!data.story.timeStart || data.story.timeStart == '0000-00-00 00:00:00')
+                        && data.task.timeStart && data.task.timeStart != '0000-00-00 00:00:00') {
+                        updateData['timeStart'] = new Date();
                     }
-                });
 
-                // Update story data
-                Story
-                    .update(
-                        {id: values.storyId},
-                        {isDone: isDone},
-                        function(error, /** sails.model.story[] */stories) {
+                    Story
+                        .update(where, updateData, function(error, stories) {
                             if (error) {
                                 cb(error);
                             } else {
                                 cb();
                             }
-                    });
-            });
+                        });
+                }
+            }
+        );
     }
 };
