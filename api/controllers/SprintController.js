7586ee470e74ca0b702df5762225f2e01853d776
tarlepp <tarmo.leppanen@kase.fi>
2013-12-04 20:00:42 +0200

    Implemented new actions to sprint controller and added necessary policies for those.

--- api/controllers/SprintController.js
+++ api/controllers/SprintController.js
@@ -205,5 +205,285 @@ module.exports = {
                 res.view(data);
             }
         }
+    },
+
+    /**
+     * Sprint charts action.
+     *
+     * @param   {Request}   req Request object
+     * @param   {Response}  res Response object
+     */
+    charts: function(req, res) {
+        var sprintId = parseInt(req.param("id"), 10);
+
+        var data = {
+            layout: req.isAjax ? "layout_ajax" : "layout"
+        };
+
+        // Get sprint and attached stories data
+        async.parallel(
+            {
+                // Fetch sprint data
+                sprint: function(callback) {
+                    DataService.getSprint(sprintId, callback);
+
+                },
+
+                // Fetch stories that are attached to this sprint
+                stories: function(callback) {
+                    DataService.getStories({sprintId: sprintId}, callback);
+                }
+            },
+            function(error, results) {
+                if (error) {
+                    res.send(500, error);
+                } else {
+                    // Store results to data
+                    data.sprint = results.sprint;
+                    data.stories = results.stories;
+
+                    // Determine story id values for task search.
+                    var storyIds = _.map(data.stories, function(story) { return {storyId: story.id}; });
+
+                    // Fetch stories tasks
+                    DataService.getTasks({or: storyIds}, function(error, tasks) {
+                        if (error) {
+                            res.send(500, error);
+                        } else {
+                            data.tasks = tasks;
+
+                            makeView();
+                        }
+                    });
+                }
+            }
+        );
+
+        function makeView() {
+            res.view(data);
+        }
+    },
+
+    /**
+     * Sprint chartDataTask action.
+     *
+     * @param   {Request}   req Request object
+     * @param   {Response}  res Response object
+     */
+    chartDataTasks: function(req, res) {
+        var sprintId = parseInt(req.param("sprintId"), 10);
+
+        var data = {};
+
+        // Get sprint and attached stories data
+        async.parallel(
+            {
+                // Fetch sprint data
+                sprint: function(callback) {
+                    DataService.getSprint(sprintId, callback);
+
+                },
+
+                // Fetch stories that are attached to this sprint
+                stories: function(callback) {
+                    DataService.getStories({sprintId: sprintId}, callback);
+                }
+            },
+            function(error, results) {
+                if (error) {
+                    res.send(500, error);
+                } else {
+                    // Store results to data
+                    data.sprint = results.sprint;
+                    data.stories = results.stories;
+
+                    // Determine story id values for task search.
+                    var storyIds = _.map(data.stories, function(story) { return {storyId: story.id}; });
+
+                    // Fetch story tasks
+                    DataService.getTasks({or: storyIds}, function(error, tasks) {
+                        if (error) {
+                            res.send(500, error);
+                        } else {
+                            data.tasks = tasks;
+
+                            parseData();
+                        }
+                    });
+                }
+            }
+        );
+
+        /**
+         * Private function to parse actual data.
+         */
+        function parseData() {
+            var initTasks = 0;
+            var storyTasks = 0;
+            var tasksOver = [];
+
+            // Iterate each stories and determine initial and "over" tasks
+            _.each(data.stories, function(story) {
+                storyTasks = 0;
+
+                // Story has not yet started, so all of its tasks are in "init" data
+                if (!(story.timeStart && story.timeStart != '0000-00-00 00:00:00')) {
+                    storyTasks = _.size(_.filter(data.tasks, function(task) {
+                        return task.storyId === story.id;
+                    }));
+                } else { // Otherwise story is either in process or done
+                    // calculate task count that are created before story start time
+                    storyTasks = _.size(_.filter(data.tasks, function(task) {
+                        return (task.storyId === story.id && task.createdAt <= story.timeStart);
+                    }));
+
+                    // Determine tasks that are added after story start
+                    tasksOver = tasksOver.concat(_.filter(data.tasks, function(task) {
+                        return (task.storyId === story.id && task.createdAt > story.timeStart);
+                    }));
+                }
+
+                // Add current story init task count to main task count
+                initTasks = initTasks + storyTasks;
+            });
+
+            data.tasksOver = _.sortBy(tasksOver, function(task) { return task.createdAt; } );
+            data.initTasks = initTasks;
+            data.chartData = [];
+
+            // Add 'Ideal task remaining' data
+            data.chartData.push({
+                name: "Ideal tasks remaining",
+                nameShort: "Ideal",
+                type: "spline",
+                color: "#3276b1",
+                dashStyle: "dash",
+                marker: {
+                    enabled: false,
+                    radius: 3
+                },
+                lineWidth: 1,
+                data: getIdealData()
+            });
+
+            // Add 'Actual task remaining' data
+            data.chartData.push({
+                name: "Actual tasks remaining",
+                nameShort: "Actual",
+                type: "spline",
+                color: "#c9302c",
+                shadow: true,
+                marker: {
+                    enabled: false,
+                    radius: 3
+                },
+                data: getActualData()
+            });
+
+            // Add 'Added tasks' data
+            data.chartData.push({
+                name: "Added tasks",
+                nameShort: "Added",
+                type: "column",
+                color: "#ec971f",
+                data: getAddedData()
+            });
+
+            res.json(data);
+        }
+
+        /**
+         * Private function to determine ideal data for charts. This is basically linear line where
+         * start point is at sprint start and value for that point is sprint initial task count. End
+         * point is at sprint end and value is zero.
+         *
+         * Output is an array of UTC date times and actual values.
+         *
+         * @returns {Array}
+         */
+        function getIdealData() {
+            var output = [];
+            var tasks = data.initTasks;
+            var startTime = data.sprint.dateStartObject();
+            var endTime = data.sprint.dateEndObject();
+            var tasksPerDay = data.initTasks / (data.sprint.durationDays() - 1);
+
+            for (var i = 0; i < (data.sprint.durationDays() - 1); i++) {
+                var date = startTime.add("days", i === 0 ? 0 : 1);
+
+                output.push([Date.UTC(date.year(), date.month(), date.date()), (tasks - i * tasksPerDay)]);
+            }
+
+            output.push([Date.UTC(endTime.year(), endTime.month(), endTime.date()), 0]);
+
+            return output;
+        }
+
+        /**
+         * Private function to get actual task data.
+         *
+         * @returns {Array}
+         */
+        function getActualData() {
+            var output = [];
+            var tasks = data.initTasks;
+            var startTime = data.sprint.dateStartObject();
+            var endTime = data.sprint.dateEndObject();
+            var tasksDone = 0;
+            var tasksOver = 0;
+
+            // Add first point of data
+            output.push([Date.UTC(startTime.year(), startTime.month(), startTime.date()), tasks]);
+
+            var currentDate = startTime.add("days", 1);
+
+            // Loops days
+            while (endTime.diff(currentDate, "days") >= 0) {
+                // Get reference date, this is used to determine actual tasks that are done or added
+                var referenceDate = currentDate.clone().subtract("days", 1);
+
+                // We are only interested days that are before current day
+                if (referenceDate.isBefore(moment().add("days", 1), "day")) {
+                    // Calculate done task count
+                    tasksDone = _.size(_.filter(data.tasks, function(task) {
+                        return task.isDone && task.timeEndObject().isSame(referenceDate, "day");
+                    }));
+
+                    // Calculate added task count
+                    tasksOver = _.size(_.filter(data.tasksOver, function(task) {
+                        return task.createdAtObject().isSame(referenceDate, "day");
+                    }));
+
+                    // Calculate new task count based to previous tasks, done and added tasks count
+                    tasks = tasks - tasksDone + tasksOver;
+
+                    output.push([Date.UTC(currentDate.year(), currentDate.month(), currentDate.date()), tasks]);
+                }
+
+                // Go to next date
+                currentDate.add("days", 1);
+            }
+
+            return output;
+        }
+
+        /**
+         * Private function to get possible added task data for current sprint.
+         *
+         * @returns {Array}
+         */
+        function getAddedData() {
+            var output = [];
+
+            _.each(_.groupBy(data.tasksOver, function(task) { return task.createdAtObject().format("YYYY-MM-DD"); }),
+                function(tasks) {
+                    var date = tasks[0].createdAtObject();
+
+                    output.push([Date.UTC(date.year(), date.month(), date.date()), _.size(tasks)]);
+                }
+            );
+
+            return output;
+        }
     }
 };
