a24ce55b54587c6ad80ecd43279fd8d4c030ea28
tarlepp <tarmo.leppanen@kase.fi>
2013-12-07 18:29:53 +0200

    Added numeral.js to response locals, so that can be used in every view. Also implemented numeral.js localization change.

--- config/express.js
+++ config/express.js
@@ -8,21 +8,29 @@
 
 var passport = require("passport");
 var moment = require("moment-timezone");
+var numeral = require("numeral");
 
 module.exports.express = {
-    customMiddleware: function (app) {
-        // Add passport middleware
+    customMiddleware: function(app) {
+        // Add passport middleware and initialize it
         app.use(passport.initialize());
         app.use(passport.session());
 
         // Add some basic data for all views
         app.use(function(req, res, next) {
             if (req.user && req.user.language) {
+                // Change moment language
                 moment.lang(req.user.language);
+
+                // Change numeral language
+                numeral.language(req.user.language, require("../node_modules/numeral/languages/" + req.user.language + ".js"));
+                numeral.language(req.user.language);
             }
 
+            // Set data to response locals, so those are accessible by any view
             res.locals.currentUser = req.user;
             res.locals.moment = moment;
+            res.locals.numeral = numeral;
 
             next();
         });
