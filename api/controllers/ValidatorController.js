22bdaf91a17047acf97475ceb4faa57db1db01cb
tarlepp <tarmo.leppanen@kase.fi>
2013-12-15 16:51:01 +0200

    Refactored ValidatorController.

--- api/controllers/ValidatorController.js
+++ api/controllers/ValidatorController.js
@@ -4,44 +4,44 @@
  * @module      :: Controller
  * @description :: Contains logic for handling requests.
  */
+"use strict";
+
 module.exports = {
     /**
-     * isUnique action. This is just a temporary solution for this...
+     * isUnique action. This will check if given attribute is unique in
+     * specified model.
      *
      * @param   {Request}   req Request object
      * @param   {Response}  res Response object
      */
     isUnique: function(req, res) {
+        var id = req.param("id");
         var model = req.param("model");
         var search = req.param("search");
-        var id = req.param("id");
 
-        // Just check that model really exists
-        try {
-            require("../models/" + model);
-        } catch (Error) {
-            res.send(404, "Invalid model");
+        // Model found
+        if (global[model] && typeof global[model] === "object") {
+            global[model]
+                .find()
+                .where(search)
+                .done(function(error, data) {
+                    if (error) {
+                        res.send(500, error);
+                    }
+
+                    var output = false;
+
+                    if (data.length === 0) {
+                        output = true;
+                    } else if (data[0].id == id) {
+                        output = true;
+                    }
+
+                    res.json(output);
+                });
+        } else {
+            res.send(404, "Invalid model")
         }
-
-        // This is dirty, very dirty...
-        eval(model)
-            .find()
-            .where(search)
-            .done(function(error, data) {
-                if (error) {
-                    res.send(500, error);
-                }
-
-                var output = false;
-
-                if (data.length === 0) {
-                    output = true;
-                } else if (data[0].id == id) {
-                    output = true;
-                }
-
-                res.json(output);
-            });
     },
 
     /**
