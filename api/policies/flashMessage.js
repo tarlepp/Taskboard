4fff2f934d791dc7c45d635ea92d8c9f6c6af14d
tarlepp <tarmo.leppanen@kase.fi>
2013-12-21 13:27:32 +0200

    Implemented flash message policy.

---
+++ api/policies/flashMessage.js
@@ -0,0 +1,44 @@
+// api/policies/flashMessage.js
+"use strict";
+
+/**
+ * Policy to check if flash messages are set from controllers.
+ *
+ * @param   {Request}   request     Request object
+ * @param   {Response}  response    Response object
+ * @param   {Function}  next        Callback function to call after policy is processed
+ */
+module.exports = function(request, response, next) {
+    var session = request.session;
+
+    /**
+     * Setter method for flash messages. This will store message to session and it will
+     * be shown in next request. Actual messages are shown via noty in client side.
+     *
+     * @param   {String}    message Message to show
+     * @param   {String}    type    Message type, defaults to "success"
+     * @param   {{}}        options Extra options for message
+     */
+    request.flash.message = function(message, type, options) {
+        type = type || "success";
+        options = options || {};
+
+        // Store message to session
+        request.session.messages.push({
+            "message": message,
+            "type": type,
+            "options": options
+        });
+    };
+
+    // Get messages from session or initialize message session
+    var messages = session.messages || (session.messages = []);
+
+    // Store current messages to res.locals so they can be accessed from views
+    response.locals({
+        flashMessages: (!messages.length) ? false : messages
+    });
+
+    // Clear existing messages from session
+    request.session.messages = [];
+};
