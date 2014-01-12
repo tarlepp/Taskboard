546dd194b9014fa07354145521ecd23c65831321
tarlepp <tarmo.leppanen@kase.fi>
2014-01-12 18:01:04 +0200

    Refactored model and added createdUserId and updatedUserId attributes.

--- api/models/Project.js
+++ api/models/Project.js
@@ -3,51 +3,64 @@
  *
  * @module      ::  Model
  * @description ::  This model represents project on taskboard.
+ * @docs        ::  http://sailsjs.org/#!documentation/models
  */
+"use strict";
+
 module.exports = {
     schema: true,
     attributes: {
         // Relation to User model
         managerId: {
-            type:       'integer',
+            type:       "integer",
             required:   true
         },
         title: {
-            type:       'string',
+            type:       "string",
             required:   true,
             minLength:  5
         },
         description: {
-            type:       'text',
+            type:       "text",
             required:   true,
-            defaultsTo: ''
+            defaultsTo: ""
         },
         dateStart: {
-            type:       'date',
+            type:       "date",
             required:   true
         },
         dateEnd: {
-            type:       'date',
+            type:       "date",
             required:   true
         },
+        createdUserId: {
+            type:       "integer",
+            required:   true
+        },
+        updatedUserId: {
+            type:       "integer",
+            required:   true
+        },
+
+        // Dynamic data attributes
 
         objectTitle: function() {
             return this.title;
         },
         dateStartObject: function() {
-            return (this.dateStart && this.dateStart != '0000-00-00')
+            return (this.dateStart && this.dateStart != "0000-00-00")
                 ? DateService.convertDateObjectToUtc(this.dateStart) : null;
         },
         dateEndObject: function() {
-            return (this.dateEnd && this.dateEnd != '0000-00-00')
+            return (this.dateEnd && this.dateEnd != "0000-00-00")
                 ? DateService.convertDateObjectToUtc(this.dateEnd) : null;
         },
         createdAtObject: function () {
-            return (this.createdAt && this.createdAt != '0000-00-00')
+            return (this.createdAt && this.createdAt != "0000-00-00 00:00:00")
                 ? DateService.convertDateObjectToUtc(this.createdAt) : null;
         },
         updatedAtObject: function () {
-            return (this.updatedAt && this.updatedAt != '0000-00-00')
+            return (this.updatedAt && this.updatedAt != "0000-00-00 00:00:00")
                 ? DateService.convertDateObjectToUtc(this.updatedAt) : null;
         }
     },
@@ -61,7 +74,7 @@ module.exports = {
      * @param   {Function}              cb
      */
     afterCreate: function(values, cb) {
-        HistoryService.write('Project', values);
+        HistoryService.write("Project", values);
 
         cb();
     },
@@ -73,7 +86,7 @@ module.exports = {
      * @param   {Function}              cb
      */
     afterUpdate: function(values, cb) {
-        HistoryService.write('Project', values);
+        HistoryService.write("Project", values);
 
         cb();
     },
@@ -88,7 +101,11 @@ module.exports = {
         Project
             .findOne(terms)
             .done(function(error, project) {
-                HistoryService.remove('Project', project.id);
+                if (error) {
+                    sails.log.error(error);
+                } else {
+                    HistoryService.remove("Project", project.id);
+                }
 
                 cb();
             });
