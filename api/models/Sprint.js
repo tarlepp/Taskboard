ed0c3d522a9909815c44c7079319fb65ed23dd11
tarlepp <tarmo.leppanen@kase.fi>
2014-01-12 18:18:45 +0200

    Refactored model and added createdUserId and updatedUserId attributes.

--- api/models/Sprint.js
+++ api/models/Sprint.js
@@ -3,31 +3,44 @@
  *
  * @module      ::  Model
  * @description ::  This model represents project sprint.
+ * @docs        ::  http://sailsjs.org/#!documentation/models
  */
+"use strict";
+
 module.exports = {
     schema: true,
     attributes: {
         // Relation to Project model
         projectId: {
-            type:       'integer',
+            type:       "integer",
             required:   true
         },
         title: {
-            type:       'string',
+            type:       "string",
             required:   true
         },
         description: {
-            type:       'text',
-            defaultsTo: ''
+            type:       "text",
+            defaultsTo: ""
         },
         dateStart: {
-            type:       'date',
+            type:       "date",
             required:   true
         },
         dateEnd: {
-            type:       'date',
+            type:       "date",
             required:   true
         },
+        createdUserId: {
+            type:       "integer",
+            required:   true
+        },
+        updatedUserId: {
+            type:       "integer",
+            required:   true
+        },
+
+        // Dynamic data attributes
 
         objectTitle: function() {
             return this.title;
@@ -36,19 +49,19 @@ module.exports = {
             return this.dateEndObject().diff(this.dateStartObject(), "days") + 1;
         },
         dateStartObject: function() {
-            return (this.dateStart && this.dateStart != '0000-00-00')
+            return (this.dateStart && this.dateStart != "0000-00-00")
                 ? DateService.convertDateObjectToUtc(this.dateStart, true) : null;
         },
         dateEndObject: function() {
-            return (this.dateEnd && this.dateEnd != '0000-00-00')
+            return (this.dateEnd && this.dateEnd != "0000-00-00")
                 ? DateService.convertDateObjectToUtc(this.dateEnd, true) : null;
         },
         createdAtObject: function () {
-            return (this.createdAt && this.createdAt != '0000-00-00')
+            return (this.createdAt && this.createdAt != "0000-00-00 00:00:00")
                 ? DateService.convertDateObjectToUtc(this.createdAt) : null;
         },
         updatedAtObject: function () {
-            return (this.updatedAt && this.updatedAt != '0000-00-00')
+            return (this.updatedAt && this.updatedAt != "0000-00-00 00:00:00")
                 ? DateService.convertDateObjectToUtc(this.updatedAt) : null;
         }
     },
@@ -62,7 +75,7 @@ module.exports = {
      * @param   {Function}              cb
      */
     afterCreate: function(values, cb) {
-        HistoryService.write('Sprint', values);
+        HistoryService.write("Sprint", values);
 
         cb();
     },
@@ -74,7 +87,7 @@ module.exports = {
      * @param   {Function}              cb
      */
     afterUpdate: function(values, cb) {
-        HistoryService.write('Sprint', values);
+        HistoryService.write("Sprint", values);
 
         cb();
     },
@@ -89,18 +102,28 @@ module.exports = {
         // Remove history data
         Sprint
             .findOne(terms)
-            .done(function(error, sprint) {
-                HistoryService.remove('Sprint', sprint.id);
+            .exec(function(error, sprint) {
+                if (error) {
+                    sails.log.error(error);
 
-                // Update all stories sprint id to 0 which belongs to delete sprint
-                Story
-                    .update(
-                        {sprintId: sprint.id},
-                        {sprintId: 0},
-                        function(error, stories) {
-                            cb();
-                        }
-                    );
+                    cb();
+                } else {
+                    HistoryService.remove("Sprint", sprint.id);
+
+                    // Update all stories sprint id to 0 which belongs to delete sprint
+                    Story
+                        .update(
+                            {sprintId: sprint.id},
+                            {sprintId: 0},
+                            function(error, stories) {
+                                if (error) {
+                                    sails.log.error(error);
+                                }
+
+                                cb();
+                            }
+                        );
+                }
             });
     }
 };
