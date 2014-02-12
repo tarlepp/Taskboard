e9d15195a623ccf386fc8ba9496046516e6d6710
tarlepp <tarmo.leppanen@kase.fi>
2014-02-12 20:09:50 +0200

    Added base of new controller + model for sprint exclude day handling #10.

---
+++ api/models/ExcludeSprintDay.js
@@ -0,0 +1,41 @@
+/**
+ * ExcludeSprintDay
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+"use strict";
+
+module.exports = {
+    schema: true,
+    attributes: {
+        sprintId: {
+            type:       "integer",
+            required:   true
+        },
+        day: {
+            type:       "date",
+            required:   true
+        },
+        createdUserId: {
+            type:       "integer",
+            required:   true
+        },
+        updatedUserId: {
+            type:       "integer",
+            required:   true
+        },
+
+        // Dynamic model data attributes
+
+        createdAtObject: function () {
+            return (this.createdAt && this.createdAt != "0000-00-00 00:00:00")
+                ? DateService.convertDateObjectToUtc(this.createdAt) : null;
+        },
+        updatedAtObject: function () {
+            return (this.updatedAt && this.updatedAt != "0000-00-00 00:00:00")
+                ? DateService.convertDateObjectToUtc(this.updatedAt) : null;
+        }
+    }
+};
