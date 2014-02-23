66570ee588ec2f9f9068c3fa6cdca4cc2b0f6c99
tarlepp <tarmo.leppanen@kase.fi>
2014-02-23 13:25:18 +0200

    Changed code styles.

--- grunt/values/injectedFiles.js
+++ grunt/values/injectedFiles.js
@@ -7,8 +7,10 @@
  * below for more options.  For this to work, you may need to install new
  * dependencies, e.g. `npm install grunt-contrib-sass`
  */
+"use strict";
+
 var cssFilesToInject = [
-  'styles/**/*.css'
+    "styles/**/*.css"
 ];
 
 /**
@@ -20,53 +22,46 @@ var cssFilesToInject = [
  */
 
 var jsFilesToInject = [
+    // Below, as a demonstration, you"ll see the built-in dependencies
+    // linked in the proper order order
 
-  // Below, as a demonstration, you'll see the built-in dependencies
-  // linked in the proper order order
+    // Bring in the socket.io client
+    "js/socket.io.js",
 
-  // Bring in the socket.io client
-  'js/socket.io.js',
+    // then beef it up with some convenience logic for talking to Sails.js
+    "js/sails.io.js",
 
-  // then beef it up with some convenience logic for talking to Sails.js
-  'js/sails.io.js',
+    // *->    put other dependencies here   <-*
 
-  // A simpler boilerplate library for getting you up and running w/ an
-  // automatic listener for incoming messages from Socket.io.
-  'js/app.js',
-
-  // *->    put other dependencies here   <-*
-
-  // All of the rest of your app scripts imported here
-  'js/**/*.js'
+    // All of the rest of your app scripts imported here
+    "js/**/*.js"
 ];
 
 /**
  * Client-side HTML templates are injected using the sources below
- * The ordering of these templates shouldn't matter.
+ * The ordering of these templates shouldn"t matter.
  * (uses Grunt-style wildcard/glob/splat expressions)
  *
  * By default, Sails uses JST templates and precompiles them into
  * functions for you.  If you want to use jade, handlebars, dust, etc.,
  * edit the relevant sections below.
  */
-
 var templateFilesToInject = [
-  'template/**/*.html'
-]
+    "template/**/*.html"
+];
 
 module.exports = {
-  // Modify css file injection paths to use
-  cssFilesToInject: cssFilesToInject.map(function(path) {
-    return '.tmp/public/' + path;
-  }),
+    // Modify css file injection paths to use
+    cssFilesToInject: cssFilesToInject.map(function(path) {
+        return ".tmp/public/" + path;
+    }),
 
-  // Modify js file injection paths to use
-  jsFilesToInject: jsFilesToInject.map(function(path) {
-    return '.tmp/public/' + path;
-  }),
+    // Modify js file injection paths to use
+    jsFilesToInject: jsFilesToInject.map(function(path) {
+        return ".tmp/public/" + path;
+    }),
 
-
-  templateFilesToInject: templateFilesToInject.map(function(path) {
-    return 'assets/' + path;
-  })
+    templateFilesToInject: templateFilesToInject.map(function(path) {
+        return "assets/" + path;
+    })
 };
