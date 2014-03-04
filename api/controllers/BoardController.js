8f5c29a2874b65df433df41b381328f5c8de81ff
tarlepp <tarmo.leppanen@kase.fi>
2014-03-04 19:54:02 +0200

    Refactored BoardController

--- api/controllers/BoardController.js
+++ api/controllers/BoardController.js
@@ -4,39 +4,54 @@
  * @module      ::  Controller
  * @description ::  Contains logic for handling requests.
  */
+"use strict";
 
 module.exports = {
     /**
+     * Overrides for the settings in `config/controllers.js`
+     * (specific to BoardController)
+     */
+    _config: {
+        blueprints: {
+            actions: false,
+            rest: false,
+            shortcuts: false
+        }
+    },
+
+    /**
      * Taskboard main action.
      *
-     * @param   {Request}   req Request object
-     * @param   {Response}  res Response object
+     * @param   {Request}   request     Request object
+     * @param   {Response}  response    Response object
      */
-    index: function(req, res) {
-        var projectId = req.route.params.projectId || 0;
-        var sprintId = req.route.params.sprintId || 0;
-        var sprintCookie = '';
+    index: function(request, response) {
+        var projectId = parseInt(request.route.params.projectId, 10);
+        var sprintId = parseInt(request.route.params.sprintId, 10);
+
+        projectId = isNaN(projectId) ? 0 : projectId;
+        sprintId = isNaN(sprintId) ? 0 : sprintId;
 
         // Project id not given, but user has cookie that contains last selected project
-        if (projectId === 0 && req.cookies.projectId) {
-            projectId = req.cookies.projectId;
+        if (projectId === 0 && request.cookies.projectId) {
+            projectId = request.cookies.projectId;
         }
 
         // Project id given and sprint id not, check cookie for sprint id
-        if (sprintId == 0 && projectId > 0) {
-            sprintCookie = 'sprintId_' + projectId;
+        if (sprintId === 0 && projectId > 0) {
+            var sprintCookie = "sprintId_" + projectId;
 
             // Cookie founded
-            if (req.cookies[sprintCookie]) {
-                sprintId = req.cookies[sprintCookie];
+            if (request.cookies[sprintCookie]) {
+                sprintId = request.cookies[sprintCookie];
             }
         }
 
-        projectId = isNaN(parseInt(projectId)) ? 0 : projectId;
-        sprintId = isNaN(parseInt(sprintId)) ? 0 : sprintId;
+        projectId = isNaN(parseInt(projectId, 10)) ? 0 : projectId;
+        sprintId = isNaN(parseInt(sprintId, 10)) ? 0 : sprintId;
 
         // Make view
-        res.view({
+        response.view({
             projectId: projectId,
             sprintId: sprintId
         });
