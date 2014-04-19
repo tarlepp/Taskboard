d36453bc715155e3cea56adfca29b1be96dd5e23
tarlepp <tarmo.leppanen@kase.fi>
2014-04-19 18:00:33 +0300

    Implemented sign in page and angular + sails.js logic for that.

--- assets/js/application.js
+++ assets/js/application.js
@@ -6,9 +6,66 @@ angular.module("TaskBoardApplication", ["TaskBoard"]);
 angular.module("TaskBoardApplication")
     .config(
         [
-            "$routeSegmentProvider", "$routeProvider",
-            function($routeSegmentProvider, $routeProvider) {
-                console.log("jee");
+            "$routeProvider", "$routeSegmentProvider", "$locationProvider", "$httpProvider",
+            function($routeProvider, $routeSegmentProvider, $locationProvider, $httpProvider) {
+
+                var checkAuthStatus = function($q, $timeout, $http, $location, $rootScope) {
+                    console.log("checkAuthStatus");
+                    // Initialize a new promise
+                    var deferred = $q.defer();
+
+                    $http
+                        .get("/Auth/Authenticated")
+                        .success(function(data, status, headers, config) {
+                            // Authenticated
+                            if (data != "false") {
+                                $timeout(deferred.resolve, 0);
+
+                                $rootScope.currentUser = data;
+                            } else { // Not Authenticated
+                                console.log(" - Not authenticated");
+                                $rootScope.message = "You need to log in.";
+
+                                $timeout(function() {
+                                    deferred.reject();
+                                }, 0);
+
+                                $location.url("/login");
+                            }
+                        })
+                        .error(function(data, status, headers, config) {
+                            $rootScope.message = "You need to log in.";
+                            $timeout(function() {
+                                deferred.reject();
+                            }, 0);
+                            $location.url("/login");
+                         });
+
+
+                    return deferred.promise;
+                };
+
+                $httpProvider.responseInterceptors.push(function($q, $location) {
+                    return function(promise) {
+                        return promise.then(
+                            // Success: just return the response
+                            function(response) {
+                                return response;
+                            },
+
+                            // Error: check the error status to get only the 401
+                            function(response) {
+                                if (response.status === 401) {
+                                    $location.url("/login");
+                                }
+
+                                return $q.reject(response);
+                            }
+                        );
+                    }
+                });
+
+
                 $routeSegmentProvider.options.autoLoadTemplates = true;
 
                 $routeSegmentProvider
@@ -31,7 +88,11 @@ angular.module("TaskBoardApplication")
 
                     .segment("board", {
                         templateUrl: "templates/board/index.html",
-                        controller: "BoardController"
+                        controller: "BoardController",
+                        resolve: {
+                            authStatus: checkAuthStatus
+                        },
+                        resolveFailed: {}
                     })
 
                     .within()
@@ -47,6 +108,23 @@ angular.module("TaskBoardApplication")
                         redirectTo: "/"
                     })
                 ;
+            }
+        ]
+    );
+
+angular.module("TaskBoardApplication")
+    .run(
+        [
+            "$rootScope", "$http",
+            function($rootScope, $http) {
+                $rootScope.message = "";
+                $rootScope.currentUser = "";
+
+                // Logout function is available in any pages
+                $rootScope.logout = function(){
+                    $rootScope.message = "Logged out.";
+                    $http.post("/logout");
+                };
             }
         ]
     );
