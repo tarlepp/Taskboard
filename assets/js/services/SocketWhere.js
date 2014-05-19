7aba795a09d230e195d4a7fbad6132c03097f879
tarlepp <tarmo.leppanen@kase.fi>
2014-05-19 20:48:36 +0300

    Added angular service for parsing socket where parameters.

---
+++ assets/js/services/SocketWhere.js
@@ -0,0 +1,53 @@
+/**
+ * Simple angular service to parse search filters for socket queries. Usage example:
+ *
+ *  $sailsSocket
+ *      .get("/Project/", {
+ *          params: {
+ *              where: SocketWhereCondition.get($scope.filters)
+ *          }
+ *      })
+ *      .success(function(response) {
+ *          // Do your data handling here
+ *      })
+ *      .error(function(response) {
+ *          // Do your error handling here
+ *      })
+ *
+ *
+ * @todo add more complex parameter handling
+ */
+'use strict';
+
+angular.module('TaskBoardServices')
+    .factory('SocketWhereCondition',
+        [
+            function() {
+                return {
+                    get: function(filters, defaults) {
+                        var output = defaults || {};
+
+                        // Get search columns
+                        var columns = _.filter(filters.columns, function(column) {
+                            return column.inSearch;
+                        });
+
+                        // Yeah we have some columns and search word so add OR condition to output
+                        if (columns.length > 0 && filters.searchWord !== '') {
+                            output.or = _.map(columns, function(column) {
+                                var temp = {};
+
+                                temp[column.column] = {
+                                    contains: filters.searchWord
+                                };
+
+                                return temp;
+                            });
+                        }
+
+                        return output;
+                    }
+                };
+            }
+        ]
+    );
