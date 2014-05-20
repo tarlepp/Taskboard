1a1d84d4f438f7ef331fe044ccfc0dacbbdaacb8
tarlepp <tarmo.leppanen@kase.fi>
2014-05-20 21:43:05 +0300

    Refactored ListSearch directive.

--- assets/js/directives/components/ListSearch.js
+++ assets/js/directives/components/ListSearch.js
@@ -31,7 +31,21 @@ angular.module('TaskBoardDirectives')
                     filters: '='
                 },
                 replace: true,
-                templateUrl: '/templates/directives/components/listSearch.html'
+                templateUrl: '/templates/directives/components/listSearch.html',
+                controller: [
+                    "$scope",
+                    function($scope) {
+                        $scope.id = Math.floor((Math.random() * 6) + 1);
+
+                        $scope.inSearch = function(item) {
+                            if (!angular.isUndefined(item.searchable)) {
+                                return item.searchable;
+                            } else {
+                                return false;
+                            }
+                        };
+                    }
+                ]
             };
         }
     );
