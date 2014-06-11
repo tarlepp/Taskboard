57d03857262ef822e190dfd3c2a4f461d856154c
tarlepp <tarmo.leppanen@kase.fi>
2014-06-11 22:45:51 +0300

    Added API for Sprint.

---
+++ backend/api/controllers/SprintController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * SprintController
+ *
+ * @description :: Server-side logic for managing Sprints
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+var _ = require('lodash');
+
+module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
+});
+
---
+++ backend/api/models/Sprint.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/**
+* Sprint.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+var _ = require('lodash');
+
+module.exports = _.merge(_.cloneDeep(require('../base/Model')), {
+    attributes: {
+        // Sprint title
+        title: {
+            type:       'string',
+            required:   true,
+            minLength:  4
+        },
+        // Description of the sprint
+        description: {
+            type:       'text',
+            defaultsTo: ''
+        },
+        // Sprint start date
+        dateStart: {
+            type:       'date',
+            required:   true
+        },
+        // Sprint end date
+        dateEnd: {
+            type:       'date',
+            required:   true
+        },
+        // Ignore weekends on sprint, this will affect to burndown charts and phase duration calculations
+        ignoreWeekends: {
+            type:       'boolean',
+            defaultsTo: false
+        },
+
+        // Below is all specification for relations to another models
+
+        // Relation to Project model
+        project: {
+            model:      'Project',
+            columnName: 'projectId',
+            required:   true
+        },
+
+        // Dynamic data attributes
+
+        // Note that this doesn't account possible sprint exclude days
+        durationDays: function() {
+            var output = 0;
+
+            if (this.ignoreWeekends) {
+                var _start = this.dateStartObject().clone();
+
+                while (this.dateEndObject().diff(_start, 'days') >= 0) {
+                    var weekDay = _start.isoWeekday();
+
+                    if (weekDay !== 6 && weekDay !== 7) {
+                        output = output + 1;
+                    }
+
+                    _start.add('days', 1);
+                }
+            } else {
+                output = this.dateEndObject().diff(this.dateStartObject(), 'days') + 1;
+            }
+
+            return output;
+        },
+
+        dateStartObject: function() {
+            return (this.dateStart && this.dateStart != '0000-00-00')
+                ? DateService.convertDateObjectToUtc(this.dateStart) : null;
+        },
+
+        dateEndObject: function() {
+            return (this.dateEnd && this.dateEnd != '0000-00-00')
+                ? DateService.convertDateObjectToUtc(this.dateEnd) : null;
+        }
+    }
+});
+
