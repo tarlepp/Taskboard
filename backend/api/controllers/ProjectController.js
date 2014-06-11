46ac4b1b7da7cb95f2bba651093773f02f48a25c
tarlepp <tarmo.leppanen@kase.fi>
2014-06-11 22:38:25 +0300

    Added API for Project.

---
+++ backend/api/controllers/ProjectController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * ProjectController
+ *
+ * @description :: Server-side logic for managing Projects
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+var _ = require('lodash');
+
+module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
+});
+
