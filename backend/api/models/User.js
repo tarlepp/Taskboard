1ad312fdea6129d574b9d362ac3021762771b67d
tarlepp <tarmo.leppanen@kase.fi>
2014-06-11 23:10:30 +0300

    Refactored User model to use generic base model and added missing relation definitions.

--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -6,33 +6,86 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
-module.exports = {
-    schema: true,
+var _ = require('lodash');
 
+module.exports = _.merge(_.cloneDeep(require('../base/Model')), {
     attributes: {
         username: {
-            type: 'string',
-            unique: true
+            type:       'string',
+            unique:     true
         },
         email: {
-            type: 'email',
-            unique: true
+            type:       'email',
+            unique:     true
         },
         firstName: {
-            type: 'string',
-            required: true
+            type:       'string',
+            required:   true
         },
         lastName: {
-            type: 'string',
-            required: true
+            type:       'string',
+            required:   true
         },
+        admin: {
+            type:       'boolean',
+            defaultsTo: false
+        },
+
+        // Below is all specification for relations to another models
+
+        // Passport configurations
         passports: {
             collection: 'Passport',
             via: 'user'
         },
-        admin: {
-            type: 'boolean',
-            defaultsTo: false
+
+        // Projects where manager
+        projectManager: {
+            collection: 'Project',
+            via: 'manager'
+        },
+
+        // Created Projects
+        createdProjects: {
+            collection: 'Project',
+            via: 'createdUser'
+        },
+
+        // Updated Projects (latest update)
+        updatedProjects: {
+            collection: 'Project',
+            via: 'updatedUser'
+        },
+
+        // Created Sprints
+        createdSprints: {
+            collection: 'Sprint',
+            via: 'createdUser'
+        },
+
+        // Updated Sprints (latest update)
+        updatedSprints: {
+            collection: 'Sprint',
+            via: 'updatedUser'
+        },
+
+        // Created Users
+        createdUsers: {
+            collection: 'User',
+            via: 'createdUser'
+        },
+
+        // Updated Users (latest update)
+        updatedUsers: {
+            collection: 'User',
+            via: 'updatedUser'
+        },
+
+        // Dynamic data attributes
+
+        // Computed user fullName string
+        fullName: function() {
+            return this.lastName + ' ' + this.firstName;
         }
     }
-};
+});
