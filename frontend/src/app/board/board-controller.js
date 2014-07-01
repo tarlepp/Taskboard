b46f2cb26579a6d179f53ce13cf240fbde275b24
tarlepp <tarmo.leppanen@kase.fi>
2014-07-01 04:00:14 +0300

    Added some debug information to main view.

--- frontend/src/app/board/board-controller.js
+++ frontend/src/app/board/board-controller.js
@@ -4,8 +4,14 @@
     angular.module('Taskboard.controllers')
         .controller('BoardController',
             [
-                '$scope', '$state', 'Auth',
-                function($scope, $state, Auth) {
+                '$scope', '$state',
+                'CurrentUser', 'Auth', 'SharedData',
+                function($scope, $state,
+                         CurrentUser, Auth, SharedData
+                ) {
+                    $scope.sharedData = SharedData.data;
+                    $scope.user = CurrentUser.user;
+                    $scope.auth = Auth;
                 }
             ]
         );
