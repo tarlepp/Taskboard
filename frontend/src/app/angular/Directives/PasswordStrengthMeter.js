5c31799e771e7b24b0f79dc761318883646d0094
tarlepp <tarmo.leppanen@kase.fi>
2014-07-14 15:23:58 +0300

    Added jquery.complexify.js library and directive to show strength of given password.

---
+++ frontend/src/app/angular/Directives/PasswordStrengthMeter.js
@@ -0,0 +1,40 @@
+/* global jQuery */
+/**
+ * Simple password strength meter directive that uses jQuery complexify plugin. Note that this directive
+ * requires 'angular-bootstrap' directives and actual complexify jQuery plugin to work.
+ *
+ * @see https://github.com/angular-ui/bootstrap
+ * @see https://github.com/danpalmer/jquery.complexify.js/
+ */
+(function() {
+    'use strict';
+
+    angular.module('Taskboard.directives')
+        .directive('passwordStrengthMeter',
+            function() {
+                return {
+                    restrict: 'E',
+                    scope: {
+                        password: '='
+                    },
+                    template: '<progressbar class="form-control" value="complexity" type="{{type}}">{{complexity|number:1}}%</progressbar>',
+                    link: function link(scope, element, attributes) {
+                        scope.complexity = 0;
+                        scope.type = 'danger';
+
+                        jQuery('#' + attributes.input).complexify({}, function(valid, complexity) {
+                            scope.complexity = complexity;
+
+                            if (complexity < 33) {
+                                scope.type = 'danger';
+                            } else if (complexity < 66) {
+                                scope.type = 'warning';
+                            } else {
+                                scope.type = 'success';
+                            }
+                        });
+                    }
+                };
+            }
+        );
+}());
