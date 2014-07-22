8195b17c115e91ab805cdf337a764da4904f9a8b
tarlepp <tarmo.leppanen@kase.fi>
2014-07-23 00:01:01 +0300

    Added 'sizeConverter' filter to convert size in bytes to human readable format.

---
+++ frontend/src/app/angular/Filters/SizeConverter.js
@@ -0,0 +1,54 @@
+/**
+ * Simple filter to convert size value (number) to human readable format. Usage example:
+ *
+ *  <span>{{sizeValueInBytes|sizeConverter:2}}</span>
+ *
+ * And that will give you something like:
+ *
+ *  <span>252 bytes</span>
+ *  <span>1.06 KB</span>
+ *  <span>2.13 MB</span>
+ *  ...
+ *
+ * Second filter parameter is optionally, if not given it defaults to 1.
+ */
+(function() {
+    'use strict';
+
+    angular.module('Taskboard.filters')
+        .filter('sizeConverter', function() {
+            return function(size, precision) {
+                precision = precision || 1;
+
+                if (size === 0 || size == null){
+                    return '';
+                }
+
+                else if(!isNaN(size)){
+                    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+                    var positionText = 0;
+
+                    if (size < 1024) {
+                        return Number(size) + ' ' + sizes[positionText];
+                    }
+
+                    while (size >= 1024) {
+                        positionText++;
+
+                        size = size / 1024;
+                    }
+
+                    var power = Math.pow (10, precision);
+                    var poweredValue = Math.ceil (size * power);
+
+                    size = poweredValue / power;
+
+                    return size + ' ' + sizes[positionText];
+                } else {
+                    console.log('Error: Not a number.');
+
+                    return '';
+                }
+            };
+        });
+}());
