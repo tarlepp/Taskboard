be266bc850908f39e67f4371463d84f7168e37e5
tarlepp <tarmo.leppanen@kase.fi>
2014-07-29 13:37:44 +0300

    Fixed timing problems on qtip directive.

--- frontend/src/app/angular/Directives/qTip.js
+++ frontend/src/app/angular/Directives/qTip.js
@@ -27,108 +27,118 @@
  */
 (function() {
     'use strict';
-
     angular.module('Taskboard.directives')
-        .directive('qtip', function() {
-            return {
-                restrict: 'A',
-                scope: {
-                    qtipId: '@',
-                    qtipContent: '@'
-                },
-                transclude: true,
-                replace: true,
-                templateUrl: '/Taskboard/partials/Directives/qTip.html',
-                link: function(scope, element, attributes) {
-                    scope.parentId = '#qtip-trigger-' + attributes.qtipId;
-                    scope.closeButton = (attributes.closeButton);
+        .directive('qtip',
+            [
+                '$timeout',
+                function($timeout) {
+                    return {
+                        restrict: 'A',
+                        scope: {
+                            qtipId: '@',
+                            qtipContent: '@'
+                        },
+                        transclude: true,
+                        replace: true,
+                        templateUrl: '/Taskboard/partials/Directives/qTip.html',
+                        link: function(scope, element, attributes) {
+                            scope.parentId = '#qtip-trigger-' + attributes.qtipId;
+                            scope.closeButton = (attributes.closeButton);
 
-                    var classes = '';
+                            var classes = '';
 
-                    if (attributes.class) {
-                        classes = attributes.class;
-                    }
-
-                    switch (attributes.position) {
-                        case 'top':
-                            scope.qtipPointerPos = 'bottom center';
-                            scope.qtipContentPos = 'top center';
-                            break;
-                        case 'bottom':
-                            scope.qtipPointerPos = 'top center';
-                            scope.qtipContentPos = 'bottom center';
-                            break;
-                        case 'bottomRight':
-                            scope.qtipPointerPos = 'top left';
-                            scope.qtipContentPos = 'bottom center';
-                            break;
-                        case 'left':
-                            scope.qtipPointerPos = 'bottom right';
-                            scope.qtipContentPos = 'top left';
-                            break;
-                        case 'leftMiddle':
-                            scope.qtipPointerPos = 'center right';
-                            scope.qtipContentPos = 'center left';
-                            break;
-                        case 'right':
-                            scope.qtipPointerPos = 'center left';
-                            scope.qtipContentPos = 'center right';
-                            break;
-                        default:
-                            scope.qtipPointerPos = 'bottom left';
-                            scope.qtipContentPos = 'top right';
-                            break;
-                    }
-
-                    jQuery('body').find(scope.parentId).each(function() {
-                        jQuery(this).qtip({
-                            metadata: {
-                                type: 'html5',      // Use HTML5 data-* attributes
-                                name: 'qtipOptions' // Grab the metadata from the data-qtip-options HTML5 attribute
-                            },
-                            content: {
-                                text: jQuery(element),
-                                title: attributes.qtipTitle,
-                                button: scope.closeButton
-                            },
-                            style: {
-                                tip: {
-                                    corner: true
-                                },
-                                classes: 'qtip-rounded qtip-shadow qtip-bootstrap ' + classes
-                            },
-                            show: {
-                                solo: true,
-                                delay: 40
-                            },
-                            hide: {
-                                delay: 50,
-                                effect: false
-                            },
-                            position: {
-                                viewport: jQuery(window),
-                                target: (attributes.target ? attributes.target : 'event'),
-                                adjust: {
-                                    method: 'flipinvert',
-                                    mouse: (attributes.static === null)
-                                },
-                                my: scope.qtipPointerPos,
-                                at: scope.qtipContentPos
-                            },
-                            events: {
-                                hide: function() {
-                                    element.qtip('destroy');
-                                }
+                            if (attributes.class) {
+                                classes = attributes.class;
                             }
-                        });
-                    });
 
-                    // Immediately destroy all tooltips belonging to the selected elements
-                    scope.$on('$destroy', function() {
-                        jQuery('body').find(scope.parentId).qtip('destroy', true);
-                    });
+                            switch (attributes.position) {
+                                case 'top':
+                                    scope.qtipPointerPos = 'bottom center';
+                                    scope.qtipContentPos = 'top center';
+                                    break;
+                                case 'bottom':
+                                    scope.qtipPointerPos = 'top center';
+                                    scope.qtipContentPos = 'bottom center';
+                                    break;
+                                case 'bottomRight':
+                                    scope.qtipPointerPos = 'top left';
+                                    scope.qtipContentPos = 'bottom center';
+                                    break;
+                                case 'left':
+                                    scope.qtipPointerPos = 'bottom right';
+                                    scope.qtipContentPos = 'top left';
+                                    break;
+                                case 'leftMiddle':
+                                    scope.qtipPointerPos = 'center right';
+                                    scope.qtipContentPos = 'center left';
+                                    break;
+                                case 'right':
+                                    scope.qtipPointerPos = 'center left';
+                                    scope.qtipContentPos = 'center right';
+                                    break;
+                                default:
+                                    scope.qtipPointerPos = 'bottom left';
+                                    scope.qtipContentPos = 'top right';
+                                    break;
+                            }
+
+                            var init = function() {
+                                jQuery('body').find(scope.parentId).each(function() {
+                                    console.log('founded');
+                                    jQuery(this).qtip({
+                                        metadata: {
+                                            type: 'html5',      // Use HTML5 data-* attributes
+                                            name: 'qtipOptions' // Grab the metadata from the data-qtip-options HTML5 attribute
+                                        },
+                                        content: {
+                                            text: jQuery(element),
+                                            title: attributes.qtipTitle,
+                                            button: scope.closeButton
+                                        },
+                                        style: {
+                                            tip: {
+                                                corner: true
+                                            },
+                                            classes: 'qtip-rounded qtip-shadow qtip-bootstrap ' + classes
+                                        },
+                                        show: {
+                                            solo: true,
+                                            delay: 40
+                                        },
+                                        hide: {
+                                            delay: 50,
+                                            effect: false
+                                        },
+                                        position: {
+                                            viewport: jQuery(window),
+                                            target: (attributes.target ? attributes.target : 'event'),
+                                            adjust: {
+                                                method: 'flipinvert',
+                                                mouse: (attributes.static === null)
+                                            },
+                                            my: scope.qtipPointerPos,
+                                            at: scope.qtipContentPos
+                                        },
+                                        events: {
+                                            hide: function() {
+                                                element.qtip('destroy');
+                                            }
+                                        }
+                                    });
+                                });
+                            };
+
+                            $timeout(function() {
+                                init();
+                            });
+
+                            // Immediately destroy all tooltips belonging to the selected elements
+                            scope.$on('$destroy', function() {
+                                jQuery('body').find(scope.parentId).qtip('destroy', true);
+                            });
+                        }
+                    };
                 }
-            };
-        }
-    );
+            ]
+        );
 }());
