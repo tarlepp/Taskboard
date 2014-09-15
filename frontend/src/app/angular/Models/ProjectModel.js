2f3003523e014b2e79bb6efadbec519d348a551a
tarlepp <tarmo.leppanen@kase.fi>
2014-09-15 21:44:54 +0300

    Refactored ProjectModel service.

--- frontend/src/app/angular/Models/ProjectModel.js
+++ frontend/src/app/angular/Models/ProjectModel.js
@@ -11,32 +11,19 @@
     angular.module('Taskboard.services')
         .factory('ProjectModel',
             [
-                '$sailsSocket', 'DataService', '_',
-                function($sailsSocket, DataService, _) {
+                '$sailsSocket',
+                '_',
+                'DataService', 'HelperService',
+                function($sailsSocket,
+                         _,
+                         DataService, HelperService
+                ) {
+                    // Determine used service variables
                     var endpoint = 'project';
-                    var project = {};
-                    var projects = [];
                     var handlers = {};
+                    var service = {};
 
-                    // Add handler for 'created' event
-                    handlers.created = function(project) {
-                        projects.push(project);
-                    };
-
-                    // Handler for 'updated' event
-                    handlers.updated = function(project) {
-                        handlers.removed(project);
-                        handlers.created(project);
-                    };
-
-                    // Handler for 'removed' event
-                    handlers.removed = function(project) {
-                        _.remove(projects, function(projectOld) {
-                            return projectOld.id === project.id;
-                        });
-                    };
-
-                    // Subscribe to 'project' endpoint and attach event handlers to it
+                    // Subscribe to specified endpoint for socket messages
                     $sailsSocket
                         .subscribe(endpoint, function(message) {
                             if (handlers[message.verb]) {
@@ -46,42 +33,142 @@
                             }
                         });
 
-                    // Load projects from server
-                    function load(parameters) {
+                    // Initialize used service cache
+                    service.project = {};
+                    service.projects = [];
+
+                    /**
+                     * Add handler for 'created' event
+                     *
+                     * @param   {socket.message.create} data
+                     */
+                    handlers.created = function(data) {
+                        service.projects.push(data);
+                    };
+
+                    /**
+                     * Handler for 'updated' event.
+                     *
+                     * @param   {socket.message.update} data
+                     */
+                    handlers.updated = function(data) {
+                        // Determine updated project item data
+                        var project = _.find(service.projects, function(project) {
+                            if (project.id == data.id) {
+                                return _.assign(project, data.data);
+                            }
+                        });
+
+                        // Remove updated item from current project collection
+                        _.remove(service.projects, function(project) {
+                            return project.id == data.id;
+                        });
+
+                        // And finally add updated project object back to collection
+                        service.projects.push(project);
+
+                        // We need to refresh project select component
+                        HelperService.refreshSelectPicker('#projectSelect');
+                    };
+
+                    /**
+                     * Handler for 'removed' event
+                     *
+                     * @param   {socket.message.remove} data
+                     */
+                    handlers.removed = function(data) {
+                        _.remove(service.projects, function(projectOld) {
+                            return projectOld.id === data.id;
+                        });
+                    };
+
+                    /**
+                     * Actual public service methods below:
+                     */
+
+                    /**
+                     * Service load method. Note that calling this multiple times will overwrite
+                     * existing service project cache.
+                     *
+                     * @param   {{}}        [parameters]    Used search parameters
+                     * @param   {Boolean}   [noStore]       No store on data service
+                     *
+                     * @returns {Promise|models.project[]}
+                     */
+                    service.load = function(parameters, noStore) {
+                        noStore = noStore || false;
+
                         return DataService
                             .collection(endpoint, parameters)
                             .then(function(response) {
-                                projects = response.data;
+                                if (!_.isArray(response.data) && _.isObject(response.data)) {
+                                    response.data = [response.data];
+                                }
 
-                                return projects;
+                                if (noStore) {
+                                    return response.data;
+                                } else {
+                                    service.projects = response.data;
+
+                                    return service.projects;
+                                }
                             });
-                    }
+                    };
 
-                    // Fetch one project from server
-                    function fetch(identifier, parameters) {
+                    /**
+                     * Method to fetch single project object from backend with specified
+                     * parameters.
+                     *
+                     * @param   {Number}    identifier      Project id
+                     * @param   {{}}        [parameters]    Extra parameters for query
+                     *
+                     * @returns {Promise|models.project}
+                     */
+                    service.fetch = function(identifier, parameters) {
                         return DataService
                             .fetch(endpoint, identifier, parameters)
                             .then(function(response) {
-                                project = response.data;
+                                service.project = response.data;
 
-                                return project;
+                                return service.project;
                             });
-                    }
+                    };
 
-                    // Return count of projects
-                    function count(parameters) {
+                    /**
+                     * Service method to get project count with specified parameters.
+                     *
+                     * @param   {{}}    [parameters]    Extra parameters for query
+                     *
+                     * @returns {Promise|models.count}
+                     */
+                    service.count = function(parameters) {
                         return DataService
                             .count(endpoint, parameters)
                             .then(function(response) {
                                 return response.data;
                             });
-                    }
-
-                    return {
-                        load: load,
-                        fetch: fetch,
-                        count: count
                     };
+
+                    /**
+                     * Service method to update specified project model.
+                     *
+                     * @param   {Number}            projectId   Project id to update
+                     * @param   {models.project}    data        Data to update
+                     *
+                     * @returns {Promise|models.project}
+                     */
+                    service.update = function(projectId, data) {
+                        return DataService
+                            .update(endpoint, projectId, data)
+                            .then(function(response) {
+                                // Make necessary update actions on service
+                                handlers.updated({data: response.data, id: response.data.id});
+
+                                return response;
+                            });
+                    };
+
+                    return service;
                 }
             ]
         );
