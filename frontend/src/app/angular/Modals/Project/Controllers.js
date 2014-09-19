3404ddbbd52d90d5705c8d2d5db81d1636566246
tarlepp <tarmo.leppanen@kase.fi>
2014-09-19 18:51:36 +0300

    Added some stuff to Project modal controller.

--- frontend/src/app/angular/Modals/Project/Controllers.js
+++ frontend/src/app/angular/Modals/Project/Controllers.js
@@ -14,17 +14,23 @@
                 '_',
                 'toastr',
                 'CurrentUser', 'ModalService',
-                'TabConfig',
+                'TabConfig', 'TabInit',
+                'ProjectModel',
                 '_project',
                 function($scope, $modalInstance,
                          _,
                          toastr,
                          CurrentUser, ModalService,
-                         TabConfig,
+                         TabConfig, TabInit,
+                         ProjectModel,
                          _project
                 ) {
+                    // Specify some default scope parameters
+                    $scope._project = _project;
                     $scope.currentUser = CurrentUser.user();
                     $scope.modalService = ModalService;
+                    $scope.objectName = 'project';
+                    $scope.objectId = $scope._project.id;
 
                     // Specify tabs
                     $scope.tabs = TabConfig.projectEdit();
@@ -33,6 +39,25 @@
                     $scope.opened = {
                         dateStart: false,
                         dateEnd: false
+                    };
+
+                    // Specify form objects
+                    $scope.form = {
+                        projectBasic: {},
+                        projectSprintSettings: {}
+                    };
+
+                    /**
+                     * Function which is triggered whenever user activates tab on user profile
+                     * modal. This will check if current tab has init function defined, and if
+                     * it has function will trigger that automatic.
+                     *
+                     * @param   {helpers.tabConfig} tab
+                     */
+                    $scope.selectTab = function(tab) {
+                        $scope.activeTab = tab;
+
+                        TabInit.init(tab);
                     };
 
                     /**
@@ -62,8 +87,50 @@
 
                     // Function to reset current project form
                     $scope.reset = function() {
-                        $scope.project = angular.copy(_project);
+                        $scope.project = angular.copy($scope._project);
                     };
+
+                    /**
+                     * Function to save current project data to database.
+                     *
+                     * @param   {boolean}   [close]
+                     */
+                    $scope.save = function(close) {
+                        close = close || false;
+
+                        $scope.$broadcast('show-errors-check-validity');
+
+                        // Yeah we have valid form
+                        if ($scope.form.projectBasic.$valid &&
+                            $scope.form.projectSprintSettings.$valid
+                        ) {
+                            $scope.saving = true;
+
+                            // Update current project object
+                            ProjectModel
+                                .update($scope.project.id, $scope.project)
+                                .then(function(response) {
+                                    $scope._project = response.data;
+
+                                    $scope.reset();
+
+                                    toastr.success('Project data updated successfully.');
+
+                                    $scope.saving = false;
+                                });
+
+                            if (close) {
+                                $scope.close();
+                            }
+                        } else {
+                            console.log('validation error');
+                        }
+                    };
+
+                    // Watcher for user object to track form changes
+                    $scope.$watch('project', function(valueNew) {
+                        $scope.formChanged = !_.isEqual(valueNew, $scope._project);
+                    }, true);
 
                     $scope.reset();
                 }
